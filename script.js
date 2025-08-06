a132ee5e9483b74ade1263174b014ef776c3351a
shiburshi <217582571+shiburshi@users.noreply.github.com>
2025-08-06 12:11:18 +0500

    Add colorful mode

--- script.js
+++ script.js
@@ -15,9 +15,17 @@ function removeGrid() {
     container.innerHTML = '';
 }
 
+function getRandomColor(){
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
 container = document.querySelector(".container");
 newBtn = document.querySelector("#new");
 transparentBtn = document.querySelector("#transparent");
+colorfulBtn = document.querySelector("#colorful");
 
 document.addEventListener("click", (e) => {
     if (e.target === newBtn) {
@@ -29,10 +37,20 @@ document.addEventListener("click", (e) => {
     if (e.target === transparentBtn){
         transparentBtn.classList.toggle("pressed");
     }
+    if (e.target === colorfulBtn){
+        colorfulBtn.classList.toggle("pressed");
+    }
 })
 
 document.addEventListener("mouseover", (e) => {
     if (e.target.classList.contains("cell")) {
+        //colorful mode
+        if (colorfulBtn.classList.contains("pressed")) {
+            if (!e.target.classList.contains("colored")) {
+                e.target.style.backgroundColor = getRandomColor();
+            };
+        }
+        //classic mode + transparency
         e.target.classList.add("colored");
         if (transparentBtn.classList.contains("pressed")) {
             e.target.style.opacity = +window.getComputedStyle(e.target).getPropertyValue("opacity") + 0.1;
